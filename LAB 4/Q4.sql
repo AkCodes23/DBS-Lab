eef4091ab36484c1d60ba1f9de6aecdb3868309a
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2025-01-28 15:09:40 +0530

    Update Q4.sql

--- LAB 4/Q4.sql
+++ LAB 4/Q4.sql
@@ -1,7 +1,7 @@
-SELECT pe.name AS OWNER_NAME, COUNT(p.report_number) AS No_of_Accidents
+SELECT pe.name AS OWNER_NAME, EXTRACT(YEAR FROM a.accd_date) AS Year, COUNT(p.report_number) AS No_of_Accidents
 FROM PERSON pe, OWNS o, PARTICIPATED p, ACCIDENT a
 WHERE pe.driver_id = o.driver_id
   AND o.regno = p.regno
   AND p.report_number = a.report_number
 GROUP BY pe.name, EXTRACT(YEAR FROM a.accd_date)
-HAVING COUNT(p.report_number) > 2;
+ORDER BY pe.name, Year;
